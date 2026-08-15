275ca086a8cf5831238719894b30ae8bfeaa96fb
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-15 23:15:02 +0530

    Implement groupAnagrams function
    Fix logic for grouping anagrams and update map handling.

--- groupAnagram.js
+++ groupAnagram.js
@@ -1,1 +1,23 @@
 
+var groupAnagrams = function (strs) {
+  let map =  new Map();
+
+  for(let i = 0; i <strs.length; i++){
+    let fr = new Array(26).fill(0);
+    let word = strs[i];
+
+    for(let j = 0; j< word.length; j++){
+      fr[word[j].charCodeAt(0)-97] += 1;
+    }
+
+    let key = fr.split("#").join();
+
+    if(map.has(key)){
+      map.get(key).push[...word];
+    }else{
+      map.set(key, []);
+    }
+  }
+
+  return [...map.values()];
+}
