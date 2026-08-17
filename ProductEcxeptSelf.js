c6b24272455bc73cbe0fcc8f2ae21471d66dd8ca
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-17 11:30:15 +0530

    Add product function for array elements excluding self
    Implement product function to calculate product of array elements excluding self using prefix and suffix arrays. Also, provide an optimized version without using extra space.

--- ProductEcxeptSelf.js
+++ ProductEcxeptSelf.js
@@ -1,1 +1,50 @@
 
+function product(nums){
+
+  // using prefix and suffix array
+
+  let prefix = new Array(nums.length).fill(1);
+  let suffix =  new Array(nums.length).fill(1);
+
+  let pre = 1;
+  for(let i = 0; i < nums.length; i++){
+    prefix[i] =  prefix[i] * pre;
+    pre = pre * nums[i];
+  }
+
+   let suf = 1;
+   for(let i = nums.length-1; i >= 0; i--){
+    suffix[i] =  suffix[i] * suf;
+    suf = suf * nums[i];
+  }
+
+  let ans = new Array(nums.length).fill(1);
+  for(let i = 0; i<nums.length; i++){
+    ans[i] = prefix[i] * suffix[i];
+  }
+
+  retrun ans;
+
+
+  // without using extra Space excluding Ans Array;
+
+
+  let ans = new Array(nums.length).fill(1);
+
+  let pre = 1; 
+  for(let i = 0;  i < nums.length; i++){
+    ans[i] *= pre;
+    pre *= nums[i];
+  }
+
+  let suf = 1;
+  for(let i = nums.length -1; i >= 0; i--){
+    ans[i] *= suf;
+    suf *= nums[i];
+  }
+
+  return ans;
+
+  
+  
+}
