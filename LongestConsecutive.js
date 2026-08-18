fe694930ae37a1d5d63690bb970f0939df6fd491
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-18 07:07:14 +0530

    Add LongestConsecutive function for sequence detection
    Implemented a function to find the longest consecutive sequence in an array using both sorting and a set approach.

--- LongestConsecutive.js
+++ LongestConsecutive.js
@@ -1,1 +1,45 @@
+function LongestConsecutive(nums){
 
+  // by sorting Array and then using counter;
+
+let sorted =   nums.sort((a, b) => a-b);
+
+  let count = 1;
+  let longest = 0;
+  for(let i = 0; i < sorted.length; i++){
+     if(sorted[i] === sorted[i + 1]){
+       continue;
+     } else if( sorted [i] +1 === sorted[i+1]){
+       count++;
+     }else{
+       longest =  Math.max(longest, count);
+       count = 0;
+     }
+  }
+  return longest;
+
+  // using set 
+
+  let set = new Set();
+
+  for (let i = 0; i < nums.length; i++){
+    set.add(nums[i]);
+  }
+
+  let longest = 0;
+
+  for(let val of set){
+    if(!set.has(val-1)){
+      let count = 1; 
+      let curr = val;
+
+      while(set.has(curr+1)){
+        count++;
+        curr++;
+      }
+      longest =  Math.max(longest, count);
+    }
+    
+  }
+  return longest;
+}
