8be164a5122fa66fd831c4aa2918307484a5984c
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-19 21:00:16 +0530

    Implement validPalindrome function
    Fix typos and implement two-pointer approach for palindrome check.

--- validPalindrome.js
+++ validPalindrome.js
@@ -1,1 +1,20 @@
 
+function validPlindrome(str){
+
+  // two pointer Approch to solve this 
+   
+  let cleanString  str.replace(/[^a-zA-Z0-9]/g,"");
+
+  let i = 0; 
+  let j = cleanStirng-1;
+
+  while(i< j){
+    
+    if(cleanString[i] !== cleanString[j]){
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
+}
