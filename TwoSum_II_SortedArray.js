bb43f437a8b7f3241e2ffb3caf674f101d9dca1c
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-19 21:05:30 +0530

    Implement twoSum function for sorted array

--- TwoSum_II_SortedArray.js
+++ TwoSum_II_SortedArray.js
@@ -1,1 +1,16 @@
+function twoSum(arr, target){
+  let i = 0; 
+  let j = arr.length-1;
 
+  while(i < j){
+
+    if(arr[i] + arr[j] < target){
+      i++;
+    }else if(arr[i] + arr[j] > target){
+      j--;
+    }else{
+      return[i, j];
+    }
+  }
+  return[-1, -1];
+}
