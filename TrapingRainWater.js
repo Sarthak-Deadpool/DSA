a3737a61725e1602da5dd2eb94b96b896775c11d
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-21 07:46:19 +0530

    Add function to calculate trapped rainwater
    Implement function to calculate trapped rainwater.

--- TrapingRainWater.js
+++ TrapingRainWater.js
@@ -1,1 +1,24 @@
 
+function traping(arr){
+
+  let i = 0;
+  let j = arr.length-1;
+
+  let maxi = arr[i];
+  let maxj = arr[j];
+
+  let water = 0;
+
+  while(i < j){
+
+    if(maxi < maxj){
+      i++;
+      maxi = Math.max(maxi, arr[i]);
+      water = water + maxi - arr[i];
+    }else{
+      j--;
+      maxj = Math.max(maxj, arr[j]);
+      water =  water + maxj - arr[j];
+    }
+  }
+}
