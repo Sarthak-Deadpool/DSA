de2327a77d855cee8b206f6fdd5270daa0965c55
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-21 07:22:38 +0530

    Add function to calculate most water in container
    Implemented a two-pointer approach to find the maximum water container.

--- containerWithMostWater.js
+++ containerWithMostWater.js
@@ -1,1 +1,26 @@
+function mostWater(arr){
 
+  // two pointer approach
+  
+  let i = 0;
+  let j = arr.length -1;
+
+  let most = 0;
+  
+  while(i < j){
+
+    let b = j-i;
+    let h = Math.min(arr[i] , arr[j]);
+    most = Math.max(most, b*h);
+
+    if(arr[i] < arr[j]){
+      i++;
+    }else if(arr[i] > arr[j]){
+      j--;
+    }else{
+      i++;
+      j--;
+    }
+  }
+  return most ;
+}
