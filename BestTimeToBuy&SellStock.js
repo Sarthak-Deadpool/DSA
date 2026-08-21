9cf192dc86e0d5cdaf35dbc102ca00d988288205
Sarthak Arya <147189153+Sarthak-Deadpool@users.noreply.github.com>
2026-08-21 22:25:39 +0530

    Implement buySellStock function to calculate profit

--- BestTimeToBuy&SellStock.js
+++ BestTimeToBuy&SellStock.js
@@ -1,1 +1,26 @@
 
+let buySellStock = (stock) => {
+    let i = 0;
+    let j = i+1;
+
+    let min = stock[i];
+    let max = stock[j];
+
+
+    while(j < stock.length){
+        if(stock[j] < min){
+            i = j;
+            min = stock[j];
+        }
+    
+          max = Math.max(max, stock[j]);
+        
+        j++;
+      
+        
+    }
+
+    return max-min;
+}
+
+console.log(buySellStock([7,1,5,3,6,4]))
